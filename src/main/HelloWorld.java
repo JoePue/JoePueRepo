99cf02f52b80d9306bc2dba7b02ed9ee94ee39d7
Joerg Puettner <joerg.puettner@saxsys.de>
2012-10-14 01:36:48 +0200

    Make a Branch thing

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World from Branch b-2012-10-14-jpu");
 	}
 
 }
